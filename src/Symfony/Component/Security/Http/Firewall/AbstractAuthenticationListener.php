a10030f62ef2ecb5ed4a95876eca7d620d3caef2
Azat Khuzhin <a3at.mail@gmail.com>
2011-10-11 15:51:46 +0400

    Update according last symfony version

--- src/Symfony/Component/Security/Http/Firewall/AbstractAuthenticationListener.php
+++ src/Symfony/Component/Security/Http/Firewall/AbstractAuthenticationListener.php
@@ -171,30 +171,6 @@ abstract class AbstractAuthenticationListener implements ListenerInterface
     {
         return $this->httpUtils->checkRequestPath($request, $this->options['check_path']);
     }
-    
-    /**
-     * Get the parameters from request
-     * Support of two nesting level
-     *
-     * @param Request $request
-     * @param string $key 
-     * @return mixed
-     */
-    protected function getRequestValue(Request $request, $key)
-    {
-        if ($newKeys = $this->getArrayKeysFromString($key)) {
-            $firstKey = array_shift($newKeys);
-            $array = $request->get($firstKey);
-
-            foreach ($newKeys as $newKey) {
-                $array = array_key_exists($newKey, $array) ? $array[$newKey] : null;
-                if (!is_array($array)) {
-                    return $array;
-                }
-            }
-        }
-        return $request->get($key);
-    }
 
     /**
      * Performs authentication.
@@ -302,26 +278,5 @@ abstract class AbstractAuthenticationListener implements ListenerInterface
         }
 
         return $this->options['default_target_path'];
-    }
-    
-    /**
-     * Get array keys from string like this "users[form][password]" => output array('users', 'form', 'password')
-     *
-     * @param string $str
-     * @return array
-     */
-    private function getArrayKeysFromString($str)
-    {
-        if (!strpos($str, '[') || !strpos($str, ']')) {
-            return null;
-        }
-        $out = array();
-        preg_match('#([^\[\]]+)\[#Uis', $str, $firstMatches);
-        preg_match_all('#\[([^\[\]]+)\]#Uis', $str, $matches, PREG_SET_ORDER);
-        array_unshift($matches, $firstMatches);
-        foreach ($matches as $match) {
-            $out[] = $match[1];
-        }
-        return $out;
     }
 }
